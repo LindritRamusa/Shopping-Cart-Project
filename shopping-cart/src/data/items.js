d7c30221d62a6e751d006d231e16039f64288887
LindritRamusa <82161409+LindritRamusa@users.noreply.github.com>
2022-04-30 23:38:01 +0200

    Second Commit: Homepage got styled to a basic style *might change later*

---
+++ shopping-cart/src/data/items.js
@@ -0,0 +1,39 @@
+const items =[
+    {
+        id: 1,
+        name: "Air Jordan 5 ",
+        price: 99.00,
+        category: 'Footwear',
+        description: "Lorem ipsum dolor sit amet. Id asperiores placeat 33 sunt eos quam tenetur cum natus magni qui perferendis nisi sit quaerat perspiciatis. Rem similique omnis eum quibusdam iure qui rerum ipsum."
+    },
+    {
+        id: 2,
+        name: "Adidas ",
+        price: 99.00,
+        category: 'Footwear',
+        description: "Lorem ipsum dolor sit amet. Id asperiores placeat 33 sunt eos quam tenetur cum natus magni qui perferendis nisi sit quaerat perspiciatis. Rem similique omnis eum quibusdam iure qui rerum ipsum."
+    },
+    {
+        id: 3,
+        name: "Denim",
+        price: 99.00,
+        category: 'Jeans',
+        description: "Lorem ipsum dolor sit amet. Id asperiores placeat 33 sunt eos quam tenetur cum natus magni qui perferendis nisi sit quaerat perspiciatis. Rem similique omnis eum quibusdam iure qui rerum ipsum."
+    },
+    {
+        id: 4,
+        name: "Prada",
+        price: 99.00,
+        category: 'Jacket',
+        description: "Lorem ipsum dolor sit amet. Id asperiores placeat 33 sunt eos quam tenetur cum natus magni qui perferendis nisi sit quaerat perspiciatis. Rem similique omnis eum quibusdam iure qui rerum ipsum."
+    },
+    {
+        id: 5,
+        name: "Calvin Klein",
+        price: 99.00,
+        category: 'Underwear',
+        description: "Lorem ipsum dolor sit amet. Id asperiores placeat 33 sunt eos quam tenetur cum natus magni qui perferendis nisi sit quaerat perspiciatis. Rem similique omnis eum quibusdam iure qui rerum ipsum."
+    },
+]
+
+export default items
--- shopping-cart/src/views/HomeView.vue
+++ shopping-cart/src/views/HomeView.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+
+    <div class="product-cards-container">
+      <ProductSummaryCard
+        v-for="product in items"
+        :key="product.id"
+        :product="product"
+        v-on:view-product="viewProduct($event)"/> 
+        
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import items from '../data/items.js'
+import ProductSummaryCard from '../components/products/ProductSummaryCard'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    ProductSummaryCard
+  },
+  data(){
+    return {
+      items: items
+    }
+  },
+  methods:{
+    viewProduct(product){
+      this.product = product
+      console.log(this.product)
+    }
   }
 }
 </script>
+
+<style lang="scss">
+  .product-cards-container{
+    display:flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>
